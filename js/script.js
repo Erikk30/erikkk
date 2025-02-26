b25b10b6e323ac79a686b886bfa3b3c257acbfa5
Erikk30 <118321466+Erikk30@users.noreply.github.com>
2025-02-26 09:53:37 +0700

    erikkk

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/*==================== scrolll sections avtive kink ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+	sections.forEach(sec =>{
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 150;
+		let height= sec.offsetHeight;
+		let id = sec.getAttribute('id');
+
+		if(top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		};
+	});
+
+	/*==================== sticki nav ====================*/
+	let header = document.querySelector('header');
+	header.classList.toogle('sticky', window.scrollY > 100);
+};
+
+/*==================== goyang ====================*/
+ ScrollReveal({
+ 	reset: true,
+ 	distance: '80px',
+ 	duration: 2000,
+ 	delay: 200
+  });
+
+ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
+ ScrollReveal().reveal('.home-img, .sercives-container, .portfolio-box, .contact form', { origin: 'bottom' });
+
+ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+
+ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+const typed = new Typed('.multiple-text',  {
+	strings: ['Ui Designer','Content Creator'],
+	typeSpeed:100,
+	backSpeed:100,
+	backSpeed:1000,
+	loop:true
+});
+
